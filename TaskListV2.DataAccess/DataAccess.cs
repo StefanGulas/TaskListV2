379908fff9a9253311d58df134d707acbb9b9e92
Stefan <stefan.gulas@gmail.com>
2020-10-05 17:26:16 +0200

    Database Connection established

---
+++ TaskListV2.DataAccess/DataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskListV2.DataAccess
+{
+    public class DataAccess
+    {
+
+    }
+}
---
+++ TaskListV2.DataAccess/HelperDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TaskListV2.DataAccess
+{
+    public class HelperDataAccess
+    {
+        public static IDbConnection Conn()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionToSql"].ConnectionString);
+
+        }
+    }
+}
+}
