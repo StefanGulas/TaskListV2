e12696f9c61ed6bf02c1fa62cfcd88cd188aab45
Stefan <stefan.gulas@gmail.com>
2020-10-14 20:28:12 +0200

    Add a Task Add field

---
+++ TaskListV2.UI/Command/CreateTaskCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using TaskListV2.Model;
+
+namespace TaskListV2.UI.Command
+{
+    public class CreateTaskCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is TaskListViewModel taskList && taskList.TaskName != null && taskList.TaskName != "")
+            {
+                taskList.Tasks.Add(new Task() { TaskName = taskList.TaskName, IsImportant = taskList.IsImportant, TaskComplete = taskList.TaskComplete });
+                DataAccess db = new DataAccess();
+                db.AddTask(taskList.TaskName, (int)taskList.Priority, taskList.TaskIsChecked);
+                taskList.TaskName = "";
+
+            }
+        }
+    }
+}
